c73c6e3cd61e5675b7694c8ad3659a960cf67fd6
GU-JQR <20210312213@squ.edu.cn>
2024-04-27 11:30:15 +0800

    2.2.5

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -16,8 +16,9 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
-    @Insert("insert into employee (name,username,password,phone,sex,id_number,creat_time,update_time,create_user,update_time,status)"+
-    "value"+
-    "#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateTime},#{status}")
+    @Insert("insert into employee " +
+            "(name,username,password,phone,sex,id_number,create_time,update_time,create_user,status) "+
+    "values "+
+    "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{status})")
     void insert(Employee employee);
 }
