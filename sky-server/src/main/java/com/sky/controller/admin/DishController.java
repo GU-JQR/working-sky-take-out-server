fef6b006d45642ed2ef17085fd2fbcc112350a2c
GU-JQR <20210312213@squ.edu.cn>
2024-05-16 20:29:46 +0800

    4.3.1

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("admin/dish")
 @Api(tags = "菜品相关接口")
@@ -22,18 +24,26 @@ public class DishController {
 
     @PostMapping
     @ApiOperation("新增菜品")
-    public Result save (@RequestBody DishDTO dishDTO){
-        log.info("新增菜品:{}",dishDTO);
+    public Result save(@RequestBody DishDTO dishDTO) {
+        log.info("新增菜品:{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
         return Result.success();
     }
 
     @GetMapping("/page")
     @ApiOperation("菜品分页查询")
-    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
-        log.info("菜品分页查询:{}",dishPageQueryDTO);
-        PageResult pageResult= dishService.pageQuery(dishPageQueryDTO);
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO) {
+        log.info("菜品分页查询:{}", dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
         return Result.success(pageResult);
 
+    }
+
+    @DeleteMapping
+    @ApiOperation("菜品批量删除")
+    public Result delete(@RequestBody List<Long> ids) {
+        log.info("菜品批量删除:{}", ids);
+        dishService.deleteBatch(ids);
+        return Result.success();
     }
 }
