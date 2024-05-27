817d95879abf7e6f878e093c969c105d5367791f
GU-JQR <20210312213@squ.edu.cn>
2024-05-27 21:08:58 +0800

    用户下单 2.0

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -7,6 +7,7 @@ import com.sky.vo.OrderSubmitVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Api("c端-订单接口")
 public class OrderController {
 
+    @Autowired
     private OrderService orderService;
 
     @PostMapping("/submit")
--- sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -2,10 +2,12 @@ package com.sky.mapper;
 
 import com.sky.entity.OrderDetail;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @Mapper
 public interface OrderDetailMapper {
-    void insertBatch(List<OrderDetail> orderDetailList);
+    void insertBatch(@Param("orderDetail") List<OrderDetail> orderDetailList);
 }
