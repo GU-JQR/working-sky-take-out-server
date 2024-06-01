4b417e42c49a2bedbed9afc7ae63d73a0eb7455d
GU-JQR <20210312213@squ.edu.cn>
2024-06-01 16:45:49 +0800

    工作台展示数据

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -13,6 +13,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -56,4 +57,12 @@ public interface DishMapper {
     List<Dish> list(Dish dish);
 
     void updata(Dish dish);
+
+    /**
+     * 根据条件统计菜品数量
+     *
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
